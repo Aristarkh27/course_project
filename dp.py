a388121240d0767a1bc31599cbbec153bce3d006
romanhse <90682899+romanhse@users.noreply.github.com>
2024-06-13 22:26:45 +0300

    Update dp.py

--- dp.py
+++ dp.py
@@ -31,6 +31,14 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS  daily_prices  (
                             close REAL,
                             high REAL,
                             low REAL)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS  minute_prices  (
+                            company_code TEXT,
+                            time TIMESTAMP,
+                            volume INTEGER,
+                            open REAL,
+                            close REAL,
+                            high REAL,
+                            low REAL)''')
 
 def insert_prices_into_db_daily(figi_index):
 
@@ -133,13 +141,42 @@ def create_df(share_name, candles : [HistoricCandle]):
                        (share_name, time, volume, open, close, high, low))
         # print(share_name, time, volume, open, close, high, low)
 
+def create_df_min(share_name, candles : [HistoricCandle]):
+
+    for c in candles:
+        time = c.time
+        volume = c.volume
+        open = cast_money(c.open)
+        close = cast_money(c.close)
+        high = cast_money(c.high)
+        low = cast_money(c.low)
+        cursor.execute("INSERT INTO  minute_prices (company_code, time, volume, open, close, high, low) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                       (share_name, time, volume, open, close, high, low))
+        conn.commit()
+        print((share_name, time, volume, open, close, high, low))
+
+
+def minute_candles(share_name):
+    now = datetime.now()
+    midnight_today = datetime(year=now.year, month=now.month, day=now.day, hour=0, minute=0, second=0)
+    with Client("t.rWXfCVP9o5JNhsgIllWtwxyNPR9dZJZIjSnZ5-xvupVLwsGLLNDa2EMuPvuJM6FzKV3M75rf_DiePZWiCra3fA") as client:
+        date = midnight_today
+        r = client.market_data.get_candles(
+            figi=share_name,
+            from_=date,
+            to=now,
+            interval=CandleInterval.CANDLE_INTERVAL_1_MIN # см. utils.get_all_candles
+        )
+        create_df_min(share_name, r.candles)
 
 codes = ['TCS00A1028C7', 'BBG00HBPXX50', 'BBG00R83FV73', 'BBG00A102RX6', 'BBG00FZMB3Y3', 'TCS00A1029T9', 'BBG00FX0RDF5', 'BBG00PVL2PM1', 'BBG00YJKHN65', 'BBG00PM8S500', 'TCS00A102DK3', 'BBG00QG2Y896', 'BBG011PCH913', 'BBG00HTSM1G5', 'BBG00NHLC0N5', 'TCS00A103VH9', 'TCS00A103VM9', 'TCS00A102EQ8']
 
 for i in codes:
     print(i)
-    insert_prices_into_db_daily(i)
-    insert_prices_into_db_hourly(i)
+    # insert_prices_into_db_daily(i)
+    # insert_prices_into_db_hourly(i)
+     minute_candles(i)
+
 
 
 def fill_table_with_latest_values_daily():
